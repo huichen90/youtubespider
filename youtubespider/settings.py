0d45d62484e57a323934d8738efd0278bc992bef
huochen <huichen90>
2018-05-15 15:06:39 +0800

    取消代理

--- youtubespider/settings.py
+++ youtubespider/settings.py
@@ -77,10 +77,10 @@ DB_CHARSET = "utf8"
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-   'youtubespider.middlewares.Youtubespiderv2DownloaderMiddleware': 543,
-    'youtubespider.middlewares.ProxyMiddleware': 100,
-}
+# DOWNLOADER_MIDDLEWARES = {
+#    'youtubespider.middlewares.Youtubespiderv2DownloaderMiddleware': 543,
+#     'youtubespider.middlewares.ProxyMiddleware': 100,
+# }
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
