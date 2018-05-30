5c3c207ddb970d09857c0daadf44b1cbec2737ed
huochen <huichen90>
2018-05-30 14:59:20 +0800

    1、优化代码机构;2、添加时间戳的比较；3、加入isdownload默认值

--- youtubespider/pipelines.py
+++ youtubespider/pipelines.py
@@ -5,23 +5,14 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import datetime
+import logging
 
 import pymysql
 from scrapy.utils.project import get_project_settings
 
 from youtubespider.videodownload import VdieoDownload
 
-
-class Youtubespiderv2Pipeline(object):
-    def process_item(self, item, spider):
-        db = pymysql.connect("127.0.0.1", "root", "root", "test", charset='utf8')
-        d = VdieoDownload(db=db)
-        d.Automatic_download(time=item['limit_time'])
-        # 关闭数据库连接
-        db.close()
-        return item
-
-class MysqlPipeline(object):
+class Mysql(object):
     """存储到数据库中"""
 
     def __init__(self):
@@ -48,28 +39,59 @@ class MysqlPipeline(object):
     def colose_spider(self,spider):
         self.conn.close()
         self.cursor.close()
-    def process_item(self,item,spider):
+
+class Youtubespiderv2Pipeline(Mysql):
+    def process_item(self, item, spider):
+        try:
+            d = VdieoDownload(db=self.conn,cursor=self.cursor)
+            d.Automatic_download()
+        except Exception as e:
+            print(e)
+            logging.error('下载失败 %s'%e)
+        return item
+
+class MysqlPipeline(Mysql):
+    """存储到数据库中"""
+    """存储到数据库中"""
+
+    def colose_spider(self, spider):
+        self.conn.close()
+        self.cursor.close()
+
+    def process_item(self, item, spider):
 
         # 查重处理
         self.cursor.execute(
-                """select * from videoitems where url = %s""",
-                item['url'])
+            """select * from videoitems where url = %s""",
+            item['url'])
         # 是否有重复数据
-        repetition = self.cursor.fetchall()
+        repetition = self.cursor.fetchone()
 
         # 重复
         if repetition:
-                print(repetition)
-                print("此条重复抓取，没有存入数据库")
-        else:
-            dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            sql = 'insert into videoitems(title,keywords,spider_time,url,site_name,video_time,play_count,upload_time,info,video_category,tags,task_id)' \
-                  ' values( "%s","%s","%s","%s", "%s" ,"%s","%s", "%s", "%s","%s","%s","%s")' \
-                  %(item['title'],item['keywords'],dt,item['url'],item['site_name'],item['video_time'],item["play_count"],item['upload_time'],item['info'],
-                    item['video_category'],item['tags'],item['task_id'],)
-            #执行SQL语句
+            print("此条重复抓取，没有存入数据库")
+        elif int(item['video_time']) > int(item['limit_time']):
+            print('视频时间太长了，没有存入数据库')
+        elif int(item['upload_time']) >= int(item['start_date']) and int(item['upload_time']) <= int(item['end_date']):
+            item['upload_time'] = self.ts2dts(item['upload_time'])
+            # dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            dt = datetime.datetime.now().strftime("%Y-%m-%d")
+            sql = 'insert into videoitems(title,keywords,spider_time,url,site_name,video_time,' \
+                  'play_count,upload_time,info,video_category,tags,task_id,isdownload)' \
+                  ' values( "%s","%s","%s","%s", "%s" ,"%s","%s", "%s", "%s","%s","%s","%s",0)' \
+                  % (item['title'], item['keywords'], dt, item['url'], item['site_name'], item['video_time'],
+                     item["play_count"], item['upload_time'], item['info'],
+                     item['video_category'], item['tags'], item['task_id'],)
+            # 执行SQL语句
             self.cursor.execute(sql)
             self.conn.commit()
-
-
+        else:
+            print('发布日期不符合要求，没有存入数据库')
         return item
+
+    def ts2dts(self, timeStamp):
+        '''timestamp translate to datestring'''
+        import time
+        timeArray = time.localtime(timeStamp)
+        datestr = time.strftime("%Y-%m-%d", timeArray)
+        return datestr
